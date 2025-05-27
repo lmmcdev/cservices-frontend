85cfad8f7ca41fd416804e2895f66c61acea9455
lmmcdev <subscription@clmmail.com>
2025-05-27 11:53:23 -0400

    azure auth utils

---
+++ src/utils/azureAuth.js
@@ -0,0 +1,17 @@
+// src/authConfig.js
+export const msalConfig = {
+    auth: {
+      clientId: "08e5a940-4349-45b0-94ce-46505e0a99a3", // 👈 desde Azure Portal
+      authority: "https://login.microsoftonline.com/7313ad10-b885-4b50-9c75-9dbbd975618f",
+      //redirectUri: "https://mydashboard.lmitservices.com/", // o tu dominio en Azure
+      redirectUri: "http://localhost:3000",
+    },
+    cache: {
+      cacheLocation: "localStorage", // o localStorage
+      storeAuthStateInCookie: false,
+    },
+  };
+  
+  export const loginRequest = {
+    scopes: ["User.Read", "User.ReadBasic.All"], // puedes agregar más scopes según tu app
+  };
