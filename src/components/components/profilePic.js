147da93740d2f5c6b6aa157f23b60fe542ec0f5b
lmmcdev <subscription@clmmail.com>
2025-05-28 16:03:22 -0400

    Revert "Update profilePic.js"
    This reverts commit ef4a8feae8f3ca37f0225a7f33bbb5768e69783c.

--- src/components/components/profilePic.js
+++ src/components/components/profilePic.js
@@ -9,10 +9,7 @@ const ProfilePic = () => {
   return (
     <Box display="flex" alignItems="center" gap={2}>
       {profilePhoto ? (
-        <Avatar src={profilePhoto} alt={user?.name || "Usuario"} sx={{
-    width: { xs: 40, sm: 56, md: 72 },
-    height: { xs: 40, sm: 56, md: 72 },
-  }}/>
+        <Avatar src={profilePhoto} alt={user?.name || "Usuario"} />
       ) : (
         <Avatar>{user?.name?.[0]}</Avatar> // Fallback
       )}
