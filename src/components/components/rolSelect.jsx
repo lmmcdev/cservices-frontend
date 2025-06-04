4064e8f8d7a3ff5ba6330e006feaa78fe1d870a0
lmmcdev <subscription@clmmail.com>
2025-06-04 11:21:34 -0400

    Create rolSelect.jsx

---
+++ src/components/components/rolSelect.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { FormControl, InputLabel, Select, MenuItem } from '@mui/material';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
+const commonStyles = {
+  fontSize: 12,
+  height: 36,
+  width: 240,
+  color: 'text.secondary',
+  '& .MuiOutlinedInput-notchedOutline': {
+    borderColor: '#ccc',
+  },
+  '&:hover .MuiOutlinedInput-notchedOutline': {
+    borderColor: '#aaa',
+  },
+  '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
+    borderColor: '#00a1ff',
+  },
+};
+
+
+const roles = [
+  'Customer Service',
+  'Supervisor',
+];
+
+export default function RolSelect({ value, onChange, label = "Role" }) {
+  return (
+    <FormControl fullWidth variant="outlined" size="small">
+      <InputLabel 
+        id="department-select-label"
+        sx={{ fontSize: 12, color: 'text.secondary' }}
+      >
+        {label}
+      </InputLabel>
+      <Select
+        labelId="department-select-label"
+        id="department-select"
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+        label={label}
+        sx={commonStyles}
+        IconComponent={ExpandMoreIcon}
+      >
+        {roles.map((dept) => (
+          <MenuItem key={dept} value={dept} sx={{ fontSize: 12 }}>
+            {dept}
+          </MenuItem>
+        ))}
+      </Select>
+    </FormControl>
+
+  );
+}
