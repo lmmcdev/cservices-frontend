4a11d8c246377a0b21fd214f9574c5e69c738eef
lmmcdev <subscription@clmmail.com>
2025-06-11 15:26:31 -0400

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -151,7 +151,7 @@ export const addNotes = async (dispatch, setLoading, ticketId, currentAgentEmail
       throw new Error(data.message || 'Error updating notes');
     }
 
-    dispatch({ type: 'UPDATE_NOTE', payload: data.message });
+    //dispatch({ type: 'UPDATE_NOTE', payload: data.message });
     return { success: true, message: data.message || 'Updated successfully' };
   } catch (err) {
     const message = err.message || 'Something went wrong';
@@ -231,7 +231,7 @@ export const updateTicketDepartment = async (dispatch, setLoading, ticketId, cur
 
 
 //transfer center
-export const updateCenter = async (dispatch, setLoading, agentEmail, formData, center) => {
+export const updateCenter = async (dispatch, setLoading, formData, center) => {
   setLoading(true);
   try {
     const response = await fetch(`https://prod-69.eastus.logic.azure.com:443/workflows/f358af2ae24b4e2e8957c471c02e7e7c/triggers/When_a_HTTP_request_is_received/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2FWhen_a_HTTP_request_is_received%2Frun&sv=1.0&sig=Ztx0ECBzmsTG6JxtrVDQX-WhrqyBQge74GDN59ng4g8`, {
