40642b1791dc8bbc85f5c0a8f7af3c53c429b2ec
lmmcdev <suscription@clmmail.com>
2025-08-29 16:12:56 -0400

    Update constants.js

--- src/utils/js/constants.js
+++ src/utils/js/constants.js
@@ -20,6 +20,8 @@ export const GROUP_IDS = {
     REMOTE: 'b5adb985-0d20-4078-916d-126b07fafeda'
   },
   HIALEAHCENTER: { AGENTS: 'efef4665-2646-407f-82aa-1d82505f3db5'},
+  CUTLERRIDGE: { AGENTS: '83359654-b4d5-4e20-ae16-ecc3bb3572b4'},
+  BIRDROAD: { SUPERVISORS: 'ba0eaf36-b9b1-4f70-8fa2-027b5d0ac346' }
 };
 
 export const DEFAULT_AGENT_GROUPS = [
@@ -42,13 +44,13 @@ export const defaultLocationOptions = [
         'HIALEAH CENTER',
         'WEST KENDALL','CUTLER RIDGE',
         'HIALEAH',
-        'Hiatus',
+        'HIATUS',
         'MARLINS PARK',
         'MIAMI GARDENS',
-        'North Miami Beach MC',
+        'NORTH MIAMI BEACH',
         'WEST PALM BEACH',
         'WESTCHESTER',
-        'Referrals', 'OTC', 'Pharmacy',
+        'REFERRALS', 'OTC', 'PHARMACY',
         'SWITCHBOARD'
 ];
 
